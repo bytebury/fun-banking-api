9af14f99e94bbfdccaf50767d0fc60c94eaedf91
Marcello <hey@marcello.codes>
2023-12-15 17:37:56 -0500

    Setup more columns

--- models/bank_model.go
+++ models/bank_model.go
@@ -2,8 +2,9 @@ package models
 
 type Bank struct {
 	AuditModel
-	Name        string `json:"name" gorm:"not null;size:50"`
+	Name        string `json:"name" gorm:"not null;size:50;uniqueIndex:idx_user_name_slug"`
 	Description string `json:"description" gorm:"not null;size:255"`
-	UserID      uint   `gorm:"not null"`
+	Slug        string `json:"slug" gorm:"not null;size:50;uniqueIndex:idx_user_name_slug"`
+	UserID      uint   `gorm:"not null;uniqueIndex:idx_user_name_slug"`
 	User        User   `gorm:"foreignKey:UserID"`
 }
--- models/customer_model.go
+++ models/customer_model.go
@@ -4,7 +4,7 @@ type Customer struct {
 	AuditModel
 	FirstName string `json:"first_name" gorm:"not null;size:15"`
 	LastName  string `json:"last_name" gorm:"not null;size:20"`
-	PIN       string `json:"pin" gorm:"not null;size:6"`
-	BankID    uint   `json:"bank_id" gorm:"not null"`
+	PIN       string `json:"pin" gorm:"not null;size:6;uniqueIndex:idx_pin_bank"`
+	BankID    uint   `json:"bank_id" gorm:"not null;uniqueIndex:idx_pin_bank"`
 	Bank      Bank   `gorm:"foreignKey:BankID"`
 }
