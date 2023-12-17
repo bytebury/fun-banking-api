d089addaae304481054b1c0341c19830e9922bdf
Marcello <hey@marcello.codes>
2023-12-17 16:19:49 -0500

    Update bank_model.go

--- models/bank_model.go
+++ models/bank_model.go
@@ -2,9 +2,9 @@ package models
 
 type Bank struct {
 	AuditModel
-	Name        string `json:"name" gorm:"not null;size:50;uniqueIndex:idx_user_name_slug"`
+	Name        string `json:"name" gorm:"not null;size:50;`
 	Description string `json:"description" gorm:"not null;size:255"`
-	Slug        string `json:"slug" gorm:"not null;size:50;uniqueIndex:idx_user_name_slug"`
-	UserID      uint   `json:"user_id" gorm:"not null;uniqueIndex:idx_user_name_slug"`
+	Slug        string `json:"slug" gorm:"not null;size:50;uniqueIndex:idx_user_slug"`
+	UserID      uint   `json:"user_id" gorm:"not null;uniqueIndex:idx_user_slug"`
 	User        User   `json:"owner" gorm:"foreignKey:UserID"`
 }
