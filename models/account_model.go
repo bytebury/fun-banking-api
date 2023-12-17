20a44dbe0595e8ba08f12ef0167723b9b3565ed3
Marcello <hey@marcello.codes>
2023-12-17 13:51:14 -0500

    Update decimals

--- models/account_model.go
+++ models/account_model.go
@@ -3,7 +3,7 @@ package models
 type Account struct {
 	AuditModel
 	Name       string   `json:"name" gorm:"not null;size:100"`
-	Balance    float64  `json:"balance" gorm:"type:decimal(10,2);not null;default:0.00"`
+	Balance    float64  `json:"balance" gorm:"type:decimal(15,2);not null;default:0.00"`
 	CustomerID uint     `json:"customer_id" gorm:"not null"`
 	Customer   Customer `json:"-" gorm:"foreignKey:CustomerID"`
 }
--- models/money_transfer_model.go
+++ models/money_transfer_model.go
@@ -3,8 +3,8 @@ package models
 type MoneyTransfer struct {
 	AuditModel
 	Description    string  `json:"description" gorm:"not null;size:255"`
-	CurrentBalance float64 `json:"current_balance" gorm:"not null;type:decimal(10,2)"`
-	Amount         float64 `json:"amount" gorm:"not null;type:decimal(10,2)"`
+	CurrentBalance float64 `json:"current_balance" gorm:"not null;type:decimal(15,2)"`
+	Amount         float64 `json:"amount" gorm:"not null;type:decimal(15,2)"`
 	Status         string  `json:"status" gorm:"not null;size:20;default:pending"`
 	AccountID      uint    `json:"account_id" gorm:"not null"`
 	Account        Account `json:"-" gorm:"foreignKey:AccountID"`
