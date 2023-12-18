43e7e0cffc0dc05495f5e80a1705c6f0a3cfd626
Marcello <hey@marcello.codes>
2023-12-18 14:06:26 -0500

    Added accounts

--- models/customer_model.go
+++ models/customer_model.go
@@ -2,9 +2,10 @@ package models
 
 type Customer struct {
 	AuditModel
-	FirstName string `json:"first_name" gorm:"not null;size:15"`
-	LastName  string `json:"last_name" gorm:"not null;size:20"`
-	PIN       string `json:"pin" gorm:"not null;default:000000;size:6;uniqueIndex:idx_pin_bank"`
-	BankID    uint   `json:"bank_id" gorm:"not null;uniqueIndex:idx_pin_bank"`
-	Bank      Bank   `json:"-" gorm:"foreignKey:BankID;constraint:OnDelete:CASCADE;"`
+	FirstName string    `json:"first_name" gorm:"not null;size:15"`
+	LastName  string    `json:"last_name" gorm:"not null;size:20"`
+	PIN       string    `json:"pin" gorm:"not null;default:000000;size:6;uniqueIndex:idx_pin_bank"`
+	BankID    uint      `json:"bank_id" gorm:"not null;uniqueIndex:idx_pin_bank"`
+	Bank      Bank      `json:"-" gorm:"foreignKey:BankID;constraint:OnDelete:CASCADE;"`
+	Accounts  []Account `json:"accounts"`
 }
