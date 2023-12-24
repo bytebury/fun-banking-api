56e8997425496794a8bd9958989d4279b6ca7d3f
Marcello <hey@marcello.codes>
2023-12-24 12:13:59 -0500

    Updates to max

--- models/money_transfer_model.go
+++ models/money_transfer_model.go
@@ -8,8 +8,8 @@ type PagingInfo struct {
 type MoneyTransfer struct {
 	AuditModel
 	Description    string  `json:"description" gorm:"not null;size:255"`
-	CurrentBalance float64 `json:"current_balance" gorm:"not null;type:decimal(15,2)"`
-	Amount         float64 `json:"amount" gorm:"not null;type:decimal(15,2)"`
+	CurrentBalance float64 `json:"current_balance" gorm:"not null;type:decimal(50,2)"`
+	Amount         float64 `json:"amount" gorm:"not null;type:decimal(50,2)"`
 	Status         string  `json:"status" gorm:"not null;size:20;default:pending"`
 	AccountID      uint    `json:"account_id" gorm:"not null"`
 	Account        Account `json:"-" gorm:"foreignKey:AccountID;constraint:OnDelete:CASCADE;"`
