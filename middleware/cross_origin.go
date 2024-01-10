e12b4ee1f7f38cf60cd473f690fb155377490f6e
Marcello <hey@marcello.codes>
2024-01-10 03:15:53 -0500

    Update cross_origin.go

--- middleware/cross_origin.go
+++ middleware/cross_origin.go
@@ -7,7 +7,7 @@ import (
 
 func CorsMiddleware() gin.HandlerFunc {
 	config := cors.DefaultConfig()
-	config.AllowOrigins = []string{"http://localhost:3000", "http://localhost:4200", "https://fun-banking.com", "https://bytebury.github.io/fun-banking-nextjs/"} // You can set this to a specific origin or origins
+	config.AllowOrigins = []string{"http://localhost:3000", "http://localhost:4200", "https://fun-banking.com", "https://bytebury.github.io"} // You can set this to a specific origin or origins
 	config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE"}
 	config.AllowHeaders = []string{"Authorization", "Content-Type"}
 	return cors.New(config)
