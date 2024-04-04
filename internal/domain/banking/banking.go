40ab87cc0d3c70322f3a04f2b65a7a86680ba127
Marcello <hey@marcello.codes>
2024-04-03 23:03:54 -0400

    add is_primary

--- internal/domain/banking/banking.go
+++ internal/domain/banking/banking.go
@@ -19,6 +19,7 @@ type Account struct {
 	CustomerID uint     `json:"customer_id" gorm:"not null"`
 	Customer   Customer `json:"customer" gorm:"foreignKey:CustomerID;constraint:OnDelete:CASCADE;"`
 	Type       string   `json:"type" gorm:"not null;default:checking"`
+	IsPrimary  bool     `json:"is_primary" gorm:"not null;default:true"`
 }
 
 type AccountMonthlySummary struct {
